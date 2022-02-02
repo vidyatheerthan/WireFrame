d6da790604873419f7081e606ffc042f8448db21
vidyatheerthan v <vidyatheerthan@gmail.com>
2022-02-02 18:36:27 +0530

    Revert "modified TitleBox to store initial values of its elements and shrink size during zoom"
    This reverts commit 2d1d76c92f2724a6bfef5484712de845d1fb9f6d.

--- Source/Controls/WFTitleBox.xaml.cs
+++ Source/Controls/WFTitleBox.xaml.cs
@@ -19,55 +19,22 @@ namespace WireFrame
 {
     public sealed partial class WFTitleBox : UserControl
     {
-        private double zoomFactor = 1.0;
-
-        private double textBlockInitialWidth, textBlockInitialHeight;
-        private double textBlockInitialCornerRadiusTopLeft, textBlockInitialCornerRadiusTopRight, textBlockInitialCornerRadiusBottomLeft, textBlockInitialCornerRadiusBottomRight;
-        private double textInitialFontSize;
-        private double boxInitialStrokeThickness;
-
         public WFTitleBox()
         {
             this.InitializeComponent();
 
-            // --
-            this.textBlockInitialWidth = _textBlock.Width;
-            this.textBlockInitialHeight = _textBlock.Height;
-
-            this.textBlockInitialCornerRadiusTopLeft = _textBlock.CornerRadius.TopLeft;
-            this.textBlockInitialCornerRadiusTopRight = _textBlock.CornerRadius.TopRight;
-            this.textBlockInitialCornerRadiusBottomLeft = _textBlock.CornerRadius.BottomLeft;
-            this.textBlockInitialCornerRadiusBottomRight = _textBlock.CornerRadius.BottomRight;
-
-            this.textInitialFontSize = _text.FontSize;
-
-            this.boxInitialStrokeThickness = _box.StrokeThickness;
-
-            // --
             this.SizeChanged += OnSizeChanged;
         }
 
         private void OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            double zoom = ((11.0 - zoomFactor) / 10.0);
-
-            if (e.NewSize.Width < _textBlock.Width || e.NewSize.Height < _textBlock.Height)
+            if(e.NewSize.Width < _textBlock.Width || e.NewSize.Height < _textBlock.Height)
             {
                 _textBlock.Visibility = Visibility.Collapsed;
             }
             else
             {
                 _textBlock.Visibility = Visibility.Visible;
-
-                _textBlock.Width = this.textBlockInitialWidth * zoom;
-                _textBlock.Height = this.textBlockInitialHeight * zoom;
-
-                _textBlock.CornerRadius = new CornerRadius (this.textBlockInitialCornerRadiusTopLeft * zoom,
-                                                            this.textBlockInitialCornerRadiusTopRight * zoom,
-                                                            this.textBlockInitialCornerRadiusBottomLeft * zoom,
-                                                            this.textBlockInitialCornerRadiusBottomRight * zoom);
-
-                _text.FontSize = this.textInitialFontSize * zoom;
 
                 Canvas.SetLeft(_textBlock, 0);
                 Canvas.SetTop(_textBlock, Canvas.GetTop(_canvas) - _textBlock.Height);
@@ -77,18 +44,11 @@ namespace WireFrame
             Canvas.SetTop(_box, 0);
             _box.Width = e.NewSize.Width;
             _box.Height = e.NewSize.Height;
-
-            _box.StrokeThickness = this.boxInitialStrokeThickness * zoom;
         }
 
         public void SetTitle(string title)
         {
             _text.Text = title;
-        }
-
-        public void SetZoomFactor(double zoomFactor)
-        {
-            this.zoomFactor = zoomFactor;
         }
     }
 }
