3dc0c2ab98ffa1e965b10f7a1288128025695b02
vidyatheerthan v <vidyatheerthan@gmail.com>
2022-02-10 20:19:30 +0530

    added a dummy WFRotationControl

---
+++ Source/Controls/WFRotationControl.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Path = Windows.UI.Xaml.Shapes.Path;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WireFrame
+{
+    public sealed partial class WFRotationControl : UserControl
+    {
+        public WFRotationControl()
+        {
+            this.InitializeComponent();
+
+            DrawArc(new Point(100, 100), 50.0, 30.0, 60.0, _canvas);
+        }
+
+        public void DrawArc(Point center, double radius, double start_angle, double end_angle, Canvas canvas)
+        {
+            Path arc_path = new Path();
+            arc_path.Stroke = new SolidColorBrush(Colors.Black);
+            arc_path.StrokeThickness = 2;
+            Canvas.SetLeft(arc_path, 0);
+            Canvas.SetTop(arc_path, 0);
+
+            start_angle = ((start_angle % (Math.PI * 2)) + Math.PI * 2) % (Math.PI * 2);
+            end_angle = ((end_angle % (Math.PI * 2)) + Math.PI * 2) % (Math.PI * 2);
+            if (end_angle < start_angle)
+            {
+                double temp_angle = end_angle;
+                end_angle = start_angle;
+                start_angle = temp_angle;
+            }
+            double angle_diff = end_angle - start_angle;
+            PathGeometry pathGeometry = new PathGeometry();
+            PathFigure pathFigure = new PathFigure();
+            ArcSegment arcSegment = new ArcSegment();
+            arcSegment.IsLargeArc = angle_diff >= Math.PI;
+            //Set start of arc
+            pathFigure.StartPoint = new Point(center.X + radius * Math.Cos(start_angle), center.Y + radius * Math.Sin(start_angle));
+            //set end point of arc.
+            arcSegment.Point = new Point(center.X + radius * Math.Cos(end_angle), center.Y + radius * Math.Sin(end_angle));
+            arcSegment.Size = new Size(radius, radius);
+            arcSegment.SweepDirection = SweepDirection.Clockwise;
+
+            pathFigure.Segments.Add(arcSegment);
+            pathGeometry.Figures.Add(pathFigure);
+            arc_path.Data = pathGeometry;
+            canvas.Children.Add(arc_path);
+        }
+    }
+}
