7141d3a0e58360bc68fc16c1a1137a774b490d5b
vidyatheerthan v <vidyatheerthan@gmail.com>
2022-02-12 08:23:13 +0530

    added CompoundShape class

---
+++ Source/Shapes/CompoundShape.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WireFrame.Shapes
+{
+    public sealed partial class CompoundShape : UserControl, INotifyPropertyChanged
+    {
+        public static readonly DependencyProperty StretchProperty = DependencyProperty.Register(nameof(Stretch), typeof(Stretch), typeof(CompoundShape), new PropertyMetadata(null));
+        public Stretch Stretch { get => (Stretch)GetValue(StretchProperty); set => SetValue(StretchProperty, value); }
+
+        // --
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // --
+
+        public CompoundShape()
+        {
+            this.InitializeComponent();
+
+            Stretch = Stretch.Fill;
+        }
+
+        private void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
